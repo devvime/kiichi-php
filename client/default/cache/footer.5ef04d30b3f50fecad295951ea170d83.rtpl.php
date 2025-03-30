627a1a9560c89081ba9ef65af15146ede7f4471a
devvime <dihapp@outlook.com>
2025-03-30 03:00:46 -0300

    fix phinx

--- client/default/cache/footer.5ef04d30b3f50fecad295951ea170d83.rtpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>    <footer></footer>
-  </main>
-  <script src="dist/js/main.js?version=<?php echo htmlspecialchars( $version, ENT_COMPAT, 'UTF-8', FALSE ); ?>"></script>
-  <script src="dist/libs/bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
